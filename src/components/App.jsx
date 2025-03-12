3412a0286d3f3e882d508e1185ecd427cd20092e
The_Viper <swagatk1004@gmail.com>
2025-03-13 01:01:21 +0530

    Add Routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, Navbar, Profile } from './';
 
 const App = () => {
   return (
-    <div>
-      App
+    <div> 
+      <CssBaseline />
+      <Navbar />
+      <main>
+      <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   )
 }
