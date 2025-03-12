d6152139b37651d7b31dcd46502c69f1cba0df13
The_Viper <swagatk1004@gmail.com>
2025-03-13 01:13:19 +0530

    Add makeStyles by mui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 
+import useStyles from './styles'
+
 import { Actors, MovieInformation, Movies, Navbar, Profile } from './';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div> 
+    <div className={classes.root}> 
       <CssBaseline />
       <Navbar />
-      <main>
+      <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Routes>
           <Route path="/" element={<Movies />} />
           <Route path="/movie/:id" element={<MovieInformation />} />
