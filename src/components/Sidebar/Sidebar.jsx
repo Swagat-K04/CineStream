e6b2b4ea5bedf20c3a4a4c01c2859164d0a395d5
The_Viper <swagatk1004@gmail.com>
2025-03-13 20:35:10 +0530

    Implement Sidebar Layout

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/styles';
+
+import useStyles from './styles';
+
+const categories = [
+  { label: 'Popular', value: 'popular' },
+  { label: 'Top Rated', value: 'top_rated' },
+  { label: 'Upcoming', value: 'upcoming' },
+];
+const genres = [
+  { label: 'Comedy', value: 'comedy' },
+  { label: 'Action', value: 'action' },
+  { label: 'Horror', value: 'horror' },
+  { label: 'Animation', value: 'animation' },
+];
+
+const redLogo = '/logo-red.png';
+const blueLogo = '/logo-blue.png';
+
+const Sidebar = ({ setMobileOpen }) => {
+  const theme = useTheme();
+  const classes = useStyles();
+
+  return (
+    <>
+      <Link to="/" className={classes.imageLink}>
+        <img 
+          className={classes.image}
+          src={theme.palette.mode === 'light' ? blueLogo : redLogo}
+          alt='CineStream logo'
+        />
+      </Link>
+      <Divider />
+      <List>
+        <ListSubheader>Categories</ListSubheader>
+        {categories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/"> 
+            <ListItem onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img src={redLogo} className={classes.genreImages} height={30} />
+              </ListItemIcon> */}
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        <ListSubheader>Genres</ListSubheader>
+        {genres.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/"> 
+            <ListItem onClick={() => {}} button>
+              {/* <ListItemIcon>
+                <img src={redLogo} className={classes.genreImages} height={30} />
+              </ListItemIcon> */}
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </>
+  )
+}
+
+export default Sidebar
