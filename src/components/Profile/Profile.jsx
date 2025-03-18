f5a688755d78cb014bbfba6d95c4f261ff9c5b04
The_Viper <swagatk1004@gmail.com>
2025-03-18 14:21:23 +0530

    Implement Logout Functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import { Box, Typography, Button } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
 
 const Profile = () => {
+  const { user } = useSelector((state) => state.user);
+
+  const favoriteMovies = [];
+  
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
+
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display="flex" justifyContent="space-between" > 
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          LogOut &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length
+        ? <Typography variant="h5">Add favorites or watchlist some movies to see them here!</Typography>
+        : (
+          <Box>
+            FAVORITE MOVIES
+          </Box>
+        )
+      }
+    </Box>
   )
 }
 
